2ff566c2aceb88ca68f659106bcd5a573e29ae89
dubious123 <62788448+dubious123@users.noreply.github.com>
2021-07-16 23:53:56 +0900

    ~Raycasting

--- New Unity Project/Assets/Script/TestCollision.cs
+++ New Unity Project/Assets/Script/TestCollision.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class TestCollision : MonoBehaviour
 {
-    //1. 나에게 rigidBody가 있어야 한다. (대신 isKinemetic은 off)
+    //1. 나혹은 상대에게rigidBody가 있어야 한다. (대신 isKinemetic은 off)
     //2. 나에게 collider가 있어야 한다.(대신 isTrigger는 off)
     //3. 상대에게 collider가 있어야 한다.(대신 isTrigger는 off)
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision !");
+        Debug.Log($"Collision @{collision.gameObject.name}");
     }
+    //1. 둘다 collider가 있어야 한다.
+    //2. 둘중 하나는 isTrigger: on
+    //3. 둘중 하나는 RigitBody가 있어야 한다
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger !");
+        
+        Debug.Log($"Trigger @ {other.gameObject.name} !");
     }
     void Start()
     {
@@ -22,6 +26,16 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
+        Vector3 look = transform.TransformDirection(Vector3.forward);
         
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+
+        foreach (RaycastHit hit in hits)
+        {
+
+            Debug.Log($"Raycast {hit.collider.gameObject.name} !");
+        }
     }
 }
